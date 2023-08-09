58dfc3372b1aee4c78b876895419dc7deb8cf84e
BIBIANCHUMSY <bbchumsy@gmail.com>
2023-08-08 22:02:47 -0700

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """queries Reddit API and returns list containing the titles of
-    all hot articles for a given subreddit
-
-    Ensure that you are not following redirects.
-
-    Returns:
-        If no results are found for given subreddit, return None
-        If not a valid subreddit, return None.
-    """
-    url_base = 'http://www.reddit.com/r/'
-    url_query = '{:s}/hot.json'.format(subreddit)
-    headers = {'user-agent': 'starcraft'}
-    r = requests.get(url_base + url_query, headers=headers)
-
-    if (r.status_code is 302):
-        print("None")
-        return
-    if (r.status_code is 404):
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """Returns a list of titles of all hot posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+            'user-agent': 'egsyquest'
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         return None
-    else:
-        r = r.json()
-        for post in r['data']['children']:
-            hot_list.append(post['data']['title'])
 
-    return recurse(subreddit, hot_list, after)
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list
